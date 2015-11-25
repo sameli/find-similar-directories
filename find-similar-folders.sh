f66e2f29797c9cfca8e6d9b0b9dd8b30bd85ccab
sameli <sohrab5888@gmail.com>
2015-11-25 19:36:26 -0400

    Using a faster checksum

--- find-similar-folders.sh
+++ find-similar-folders.sh
@@ -16,7 +16,9 @@ find ./* -type d -print0 | while read -d $'\0' file; do
 	#basename1=$(basename "$file")
 	fileSize=$(du -h --max-depth=0 "$file")
 	#This checksum counts every byte in files and folders:
-	#checksum=$(cd "$file" && find . -type f -print0 | while read -d $'\0' fileInner; do checksumInner=$(cksum "$fileInner"); echo "$checksumInner"; done | cksum && cd ..)c
+	#checksum=$(cd "$file" && find . -type f -print0 | while read -d $'\0' fileInner; do checksumInner=$(cksum "$fileInner"); echo "$checksumInner"; done | cksum && cd ..)
+	#This checksum counts only file and folder names
+	checksum=$(cd "$file" && ls -R | cksum && cd ..)
 	echo -e "$fileSize\t$checksum" >> $pathToSaveFile/tmpfile
 
 done
