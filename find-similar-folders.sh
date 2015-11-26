6356f383b9a2a83dabb5c3fc44da5c2f26f627be
sameli <sohrab5888@gmail.com>
2015-11-26 11:34:32 -0400

    Add counter for directories

--- find-similar-folders.sh
+++ find-similar-folders.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+#./run.sh sourceDir dirToSaveTmpfile
+#sort -rh tmpfile | awk -f duplicates.awk | head
+
 #sourceDir=$1
 #sourceBaseName=$(basename "$sourceDir")
 pathToSearch=$1
 pathToSaveFile=$(pwd $2)
-
 #exit 0
 #files=$(find "$pathToSearch" -type d)
 #list=()
 #listBasename=()
+counter=0
+rm -f tmpfile
 echo -e "\n------------- path to search: $pathToSearch"
+echo -e "counting dirs"
+countdirs=$(find "$pathToSearch" -type d | wc -l)
 echo -e "main process:"
 cd "$pathToSearch"
 find ./* -type d -print0 | while read -d $'\0' file; do
@@ -20,8 +26,17 @@ find ./* -type d -print0 | while read -d $'\0' file; do
 	#This checksum counts only file and folder names
 	checksum=$(cd "$file" && ls -R | cksum && cd ..)
 	echo -e "$fileSize\t$checksum" >> $pathToSaveFile/tmpfile
+	((counter++))
 
+	if [ $(($counter % 100)) -eq 0 ]; then
+		echo "$counter of $countdirs"
+	fi
+	#list+=("$file")
+	#listBasename+=("$file1")
+	#echo -e "basename: $basename \n"
 done
+
+
 
 echo -e "------------- end at $(date)"
 
